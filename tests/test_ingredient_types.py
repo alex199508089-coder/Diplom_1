d322b49a751167cfadaf812c7f69c3cf68198b95
alex199508089-coder <alex199508089@gmail.com>
2026-09-13 21:24:46 +0400

    New_commit3

--- tests/test_ingredient_types.py
+++ tests/test_ingredient_types.py
@@ -1,6 +1,12 @@
+import pytest
+
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
-def test_ingredient_type_constants():
-    assert INGREDIENT_TYPE_SAUCE == 'SAUCE'
-    assert INGREDIENT_TYPE_FILLING == 'FILLING'
+class TestIngredientTypes:
+    @pytest.mark.parametrize("constant, expected_value", [
+        (INGREDIENT_TYPE_SAUCE, 'SAUCE'),
+        (INGREDIENT_TYPE_FILLING, 'FILLING'),
+    ])
+    def test_ingredient_type_constant(self, constant, expected_value):
+        assert constant == expected_value
